c3d11f1575d37980d4ed5d51d5ef006e4916b7ea
balajibalajanc <balajibalajanci@gmail.com>
2020-10-09 22:35:22 +0530

    New page added

--- src/app.js
+++ src/app.js
@@ -6,12 +6,13 @@ const { request } = require('http');
 const public_dir_path=path.join(__dirname,'../public');
 const view_dir_path=path.join(__dirname,'../Templates/views');
 const partials_path=path.join(__dirname,'../Templates/partials');
+
+console.log( view_dir_path);
 const port=process.env.PORT || 3000
 const app=express()
 app.set('view engine','hbs');
 app.set('views',view_dir_path);
 hbs.registerPartials(partials_path);
-
 
 app.use(express.static(public_dir_path))
 app.get('/',(req,res)=>
@@ -19,6 +20,11 @@ app.get('/',(req,res)=>
     res.render('index',{
         title:'Hi successfully done Dyamic implementation'
     });
+})
+
+app.get('/about',(req,res)=>
+{
+res.render('about',{title :'FAQ',desc:' balaji &  co'});
 })
 
 
