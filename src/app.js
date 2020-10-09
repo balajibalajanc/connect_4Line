dafbbbbe60b899c230d11cc7503245d36b293ec3
balajibalajanc <balajibalajanci@gmail.com>
2020-10-09 20:03:11 +0530

    testing2

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
-const { request } = require("express");
-
 const path= require('path');
 const express=require('express');
 const hbs=require('hbs');
-
+const { request } = require('http');
 
 const public_dir_path=path.join(__dirname,'../public');
 const view_dir_path=path.join(__dirname,'../Templates/views');
 const partials_path=path.join(__dirname,'../Templates/partials');
 const port=process.env.PORT || 3000
 const app=express()
-
 app.set('view engine','hbs');
 app.set('views',view_dir_path);
 hbs.registerPartials(partials_path);
+
 
 app.use(express.static(public_dir_path))
 app.get('',(req,res)=>
